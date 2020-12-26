a707293a08abcba1ee54c2503b3dde102a353cda
zhangyunan <zyndev@gmail.com>
2020-12-26 16:31:56 +0800

    first commit

---
+++ src/test/java/com/example/aries/AriesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
